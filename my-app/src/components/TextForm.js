6185c2abe3203d321ab61cca3843ec2ed9c02953
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-05-16 16:49:36 +0530

    My App

---
+++ my-app/src/components/TextForm.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase", "success");
+  };
+  const handleLoClick = () => {
+    console.log("lowercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase", "success");
+  };
+  const handleClearClick = () => {
+    console.log("clear was clicked" + text);
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared", "success");
+  };
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Copied to Clipboard", "success");
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h1> {props.heading}</h1>
+        <div class="mb-3">
+          <label for="myBox" class="form-label">
+            Example textarea
+          </label>
+          <textarea
+            class="form-control"
+            id="myBox"
+            rows="3"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#13466e" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy Text
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h1>Your text summary</h1>
+        <p>
+          {" "}
+          {
+            text.split(/\s+/).filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          Minutes read
+        </p>
+        <h1>Preview</h1>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview it here"}
+        </p>
+      </div>
+    </>
+  );
+}
